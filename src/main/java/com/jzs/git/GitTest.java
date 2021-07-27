0272d7080dbe42ff16c79789ab73e59797d32ca6
zhongshun <1071673478@qq.com>
2021-07-27 23:50:48 +0800

    second commit

--- src/main/java/com/jzs/git/GitTest.java
+++ src/main/java/com/jzs/git/GitTest.java
@@ -7,5 +7,6 @@ package com.jzs.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git22222");
     }
 }
