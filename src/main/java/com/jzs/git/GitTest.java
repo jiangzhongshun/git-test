0a3d5161594310a825d1b0409e5a83790006fe20
zhongshun <1071673478@qq.com>
2021-07-27 23:48:00 +0800

    first commit

---
+++ src/main/java/com/jzs/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jzs.git;
+
+/**
+ * @author zhongshun
+ * @date 2021/7/27 23:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
